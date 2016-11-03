aabe0b8be62d1b7f9da5e5004f599e533e132e60
Nikita Titov <nikitaunmortal@gmail.com>
2016-11-03 12:54:50 +0300

    Добавлена проверка входящий значений, тестирующий метод, убрана ненужная функция печати в методе сравнения.

--- CharRepeat.java
+++ CharRepeat.java
@@ -1,30 +1,29 @@
-import java.util.ArrayList;
-import java.util.HashSet;
 
 public class CharRepeat {
-    public static void outRepeat(String incomeString) {
-        char[] incomeStringToChar = incomeString.toCharArray();
-        HashSet<Character> hasThisChar = new HashSet<>();
+    public static int repeat(String[] income) {
+        if (income == null) {
+            throw new IllegalArgumentException();
+        }
 
-        for (int i = 0; i < incomeStringToChar.length; i++) {
-            if (hasThisChar.contains(incomeStringToChar[i])) {
-                continue;
-            } else {
-                hasThisChar.add(incomeStringToChar[i]);
+        for (int i = 0; i < income.length; i++) {
+            if (income[i] == null) {
+                throw new IllegalArgumentException();
             }
-            ArrayList<Integer> positions = new ArrayList<>();
-            positions.add(i);
-            for (int j = i + 1; j < incomeStringToChar.length; j++) {
+        }
 
-                if (incomeStringToChar[i] == incomeStringToChar[j]) {
-                    positions.add(j);
+        int countOfIterations = 0;
+        for (int i = 0; i < income.length - 1; ++i) {
+            for (int j = i + 1; j < income.length; ++j) {
+                if (income[i].toLowerCase().equals(income[j].toLowerCase())) {
+                    ++countOfIterations;
                 }
             }
-            System.out.print(incomeStringToChar[i] + " in position ");
-            for (Integer p : positions) {
-                System.out.print(p + " ");
-            }
-            System.out.println();
         }
+        return countOfIterations;
+    }
+
+    public static boolean testRepeatMethod(String[] income, int answer) {
+        int answerFromMethod = repeat(income);
+        return answerFromMethod == answer;
     }
 }
--- MainStartClass.java
+++ MainStartClass.java
@@ -1,6 +1,8 @@
 
 public class MainStartClass {
     public static void main(String[] args) {
-        CharRepeat.outRepeat("определяет, что в массиве строк есть одинаковые элементы");
+        String[] test = {"ad","ad","cd","dd"};
+        System.out.println(CharRepeat.repeat(test));
+        System.out.println(CharRepeat.testRepeatMethod(test, 1));
     }
 }
